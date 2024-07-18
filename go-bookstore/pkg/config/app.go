064889f0a9e90719b0f6d0b026ea4883a12c687b
ng anh <65496205+nganhcc@users.noreply.github.com>
2024-07-18 10:17:23 +0700

    Update app.go
    Hide username and password

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -8,10 +8,8 @@ import (
 var (
 	db *gorm.DB
 )
-//to connect directly to mysql by cmd; use command: mysql -u anhdev -p anhDB
-//then type your password
 func Connect() {
-	d, err := gorm.Open("mysql", "anhdev:30122004AAbb@@@/anhDB?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "username:pass@/databasename?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
